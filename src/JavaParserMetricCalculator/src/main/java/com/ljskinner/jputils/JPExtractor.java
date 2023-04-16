19079d5b28428d855faabe6892fef0344850f273
Luke S <hyperconix@pm.me>
2023-04-16 21:13:59 +0100

    feat(jputil): add new metric for counting num comparison operators
    This metric will count the number of comparison operators in a method. That is the operators >, >=, <, <= and ==.

--- src/JavaParserMetricCalculator/src/main/java/com/ljskinner/jputils/JPExtractor.java
+++ src/JavaParserMetricCalculator/src/main/java/com/ljskinner/jputils/JPExtractor.java
@@ -627,6 +627,39 @@ public class JPExtractor {
 	}
 
 	/**
+	 * This method will return the number of comparison operators in the provided method.
+	 * This will count >, >=, <, <= and ==.
+	 * <p>
+	 * You can use the helper method provided to obtain a Method Declaration node, this should
+	 * work on all fully formed method names.
+	 *
+	 * @param methodDeclaration The method declaration node that represents the method to calculate this metric from.
+	 *
+	 * @see #findMethodDeclarationNode(String)
+	 *
+	 * @return The number of comparison operators (>, >=, <, <=, ==) in the provided method
+	 */
+	public int numberOfComparisonOperatorsIn(MethodDeclaration methodDeclaration) {
+		int numComparisonOperators = 0;
+
+		List<BinaryExpr> binaryExprs = methodDeclaration.findAll(BinaryExpr.class);
+
+		for(BinaryExpr binaryExpr : binaryExprs) {
+			boolean isComparisonOperator = binaryExpr.getOperator() == BinaryExpr.Operator.GREATER ||
+					                       binaryExpr.getOperator() == BinaryExpr.Operator.GREATER_EQUALS ||
+					                       binaryExpr.getOperator() == BinaryExpr.Operator.LESS ||
+					                       binaryExpr.getOperator() == BinaryExpr.Operator.LESS_EQUALS ||
+					                       binaryExpr.getOperator() == BinaryExpr.Operator.EQUALS;
+
+			if(isComparisonOperator) {
+				numComparisonOperators++;
+			}
+		}
+
+		return numComparisonOperators;
+	}
+
+	/**
 	 * 
 	 * This method will return the number of all which match the specified type, so
 	 * long as it extends JavaParser's Node class.
